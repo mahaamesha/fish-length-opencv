4ae95af7aebee66465ba363438eb1ae7d28d926e
mahaamesha <mahaamesha@gmail.com>
2022-04-22 19:33:45 +0700

    update command

--- src/command.py
+++ src/command.py
@@ -6,5 +6,3 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to the input image")
 ap.add_argument("-w", "--width", type=float, required=True, help="width of the left-most object in the image (in inches)")
 args = vars(ap.parse_args())
-
-tes = 1
