e4ee478462bb5111346485f63805a30fd8844049
mahaamesha <mahaamesha@gmail.com>
2022-05-01 17:08:50 +0700

    watershed is works

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import src.mafunction as f
+import src.segmentation as s
 
 # Use command in terminal to use main.py
 #python main.py -i img/lele.jpg -w 1
 
-# Segmentation process
-
+# Image segmentation process
+# Have been imported in mafunction.py
+f.show_img('opening', s.opening)
+f.show_img('sure_bg', s.sure_bg)
+f.show_img('dist_traf', s.dist_transform)
+f.show_img('sure_fg', s.sure_fg)
+f.show_img('unknown', s.unknown)
+f.show_img('immg', s.immg)
 f.save_imgjson()
 
 f.encode_imgjson()
@@ -14,10 +21,11 @@ f.encode_imgjson()
 #f.fit_line()
 f.fit_poly()
 f.get_fish_length()
-f.validate_fish_length()
+#f.validate_fish_length()
 
 f.plot_curve2img()
 
 
 f.show_imgjson()
 #f.decode_imgjson()
+print("Total number of contours: ", len(s.cnts))
--- src/segmentation.py
+++ src/segmentation.py
@@ -1,20 +1,15 @@
 import json
 import cv2 as cv
 import imutils
+from matplotlib import markers
 import numpy as np
 import src.command as cmd
 
 
+
+
 # SETUP PARAMETER
 param = []
-
-#config_segmentation(param=s.param, configChoice=0)
-    # 0 (51, 51), blurSize
-    # 1 (0, 255), threshVal
-    # 2 (3, 3),   kernelSize
-    # 3 (3, 1),   erodilaIter
-    # 4 (0, 255)  cannyThresh
-
 def config_segmentation(param=param, configChoice=0):    # import tmp/config.json
     with open('tmp/config.json', 'r') as fp:
         data = json.load(fp)
@@ -32,6 +27,12 @@ def config_segmentation(param=param, configChoice=0):    # import tmp/config.jso
     return param
 
 config_segmentation()
+#config_segmentation(param=s.param, configChoice=0)
+    # 0 (51, 51), blurSize
+    # 1 (0, 255), threshVal
+    # 2 (3, 3),   kernelSize
+    # 3 (3, 1),   erodilaIter
+    # 4 (0, 255)  cannyThresh
 # (END) SETUP PARAMETER
 
 
@@ -49,6 +50,33 @@ kernel = np.ones( param[2], np.uint8 )
 erodila = cv.erode(thresh, kernel, iterations=param[3][0])
 erodila = cv.dilate(erodila, kernel, iterations=param[3][1])
 
+
+# WATERSHED
+# Remove noise
+opening = cv.morphologyEx(erodila, cv.MORPH_CLOSE, kernel, iterations=3)
+# Sure background area
+sure_bg = cv.dilate(erodila, kernel, iterations=11)
+# Find sure foreground area
+dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 3)
+ret, sure_fg = cv.threshold(dist_transform, 0.2*dist_transform.max(), 255, 0)
+# Find unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv.subtract(sure_bg, sure_fg)
+
+# Mark label
+ret, markers = cv.connectedComponents(sure_fg)
+# Add one to all labels so that sure_bg is not 0, but 1
+markers = markers + 1
+# Mark the region of unknown with 0
+markers[unknown == 255] = 0
+# Apply watershed
+immg = image.copy()
+markers = cv.watershed(immg, markers)
+# Mark boundary region with -1
+immg[markers == -1] = [0, 255, 0]
+
+
+
 # Detect the edge from BLACK WHITE img
 edged = cv.Canny(erodila, param[4][0], param[4][1])
 
@@ -60,9 +88,8 @@ res = cv.bitwise_and(image,image, mask=erodila)
 
 # IMAGE PROCESSING
 # Find many contour from BLACK WHITE img
-cnts = cv.findContours(erodila, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cnts = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
-print("Total number of contours: ", len(cnts))
 
 contoured = image.copy()
 # Draw all contours in an img
