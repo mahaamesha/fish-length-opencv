5371dee05f00b1f42fbaee56de26164a7b65161e
mahaamesha <mahaamesha@gmail.com>
2022-05-07 19:47:22 +0700

    encode in last process

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ import src.segmentation as s
 # Initialize images.json
 f.generate_imagesjson()
 
-# Save & encode all img
+# Save the img to imgcv/
 f.save_imgjson()
-f.encode_imgjson()
 
 # Create curve for each contour and save their points to points.json
 f.fit_poly()
@@ -29,8 +28,10 @@ f.generate_resultjson()
 # Plot curve to img with the result as title
 f.plot_curve2img()
 f.numbering_curve()
-# Swow img whose _flag == 1
+# Swow img whose _flagshow == 1
 f.show_imgjson()
+# Encode img whose _encodeflag == 1
+f.encode_imgjson()
 
 print("Total number of fish:", num_fish)
 print("Average of fish length:", fishlength)
