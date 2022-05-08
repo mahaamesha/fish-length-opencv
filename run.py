04a8041a41611ca884c96c975cce8e10a779406e
mahaamesha <mahaamesha@gmail.com>
2022-05-08 12:26:59 +0700

    can run repeatedly for every img in img/

--- run.py
+++ run.py
@@ -1,28 +1,27 @@
 import os, json
 
 #path = 'test/0507_02/img'
-path = 'imgtest'
+path = 'img'
 
 list = []
 # Scan all file.jpg in img/
 for (root, dirs, file) in os.walk(path):
 	for f in file:
 		if '.jpg' in f:
-            # Extract only filename
-			f = f.replace('.jpg', '')
 			list.append(f)
 
 print(list)
 
-for i in list:
+for f in list:
 	# cmd /k : remain after run
 	# cmd /c : terminate after run
-	os.system('cmd /c "python main.py -i " + path + "/%s.jpg"' %i)
+	os.system('cmd /c "python main.py -i img/%s"' %f)
+	print('cmd /c "python main.py -i img/%s"' %f)
 
 	#tmp_num = []
 	#tmp_length = []
 	#tmp_encod = []
-	print('tampung nilai num_fish & fishlength & encoded_string')	# ada 5 data untuk setiap list
+	#print('tampung nilai num_fish & fishlength & encoded_string')	# ada 5 data untuk setiap list
 	
 	# Create tmp.json
 	# Access data from result.json in fish-length-opencv
