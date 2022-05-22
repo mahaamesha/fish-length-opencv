5845f188388f4246dd573298bca5f830be0c03f0
mahaamesha <mahaamesha@gmail.com>
2022-05-22 14:06:14 +0700

    add run.py

---
+++ run.py
@@ -0,0 +1,30 @@
+import os
+
+from src.mafunction import add_encode_resultjson, clear_json_file, update_files_from_resultjson, validate_resultjson
+
+#path = 'test/0507_02/img'
+path = 'img'
+
+list = []
+# Scan all file.jpg in img/
+for (root, dirs, file) in os.walk(path):
+	for f in file:
+		if '.jpg' in f:
+			list.append(f)
+
+clear_json_file('result')
+for f in list:
+	# cmd /k : remain after run
+	# cmd /c : terminate after run
+	os.system('cmd /c "python main.py -i img/%s"' %f)
+
+	# In this step, in result.json, "encoded": ""
+	# Add the encoded string to result.json for each image
+	add_encode_resultjson(key_name=f)
+
+# result.json will save all measurements result for each file in img/
+# Then validate it, only remain RESULT key
+validate_resultjson()
+
+# From final result.json, I need to update final.bin & final.jpg
+update_files_from_resultjson()
