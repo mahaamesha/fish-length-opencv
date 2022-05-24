7fe5de669ba33ee18f4c5f2e5725aaaf92553305
mahaamesha <mahaamesha@gmail.com>
2022-05-24 14:16:53 +0700

    wrap it as a function

--- run.py
+++ run.py
@@ -1,32 +1,41 @@
 import os
 from src.mafunction import get_path_relative_to_src, add_encode_resultjson, clear_json_file, update_files_from_resultjson, validate_resultjson, print_final_result
 
-# Get the path of this file --> ~/fish-length-opencv/
-path = get_path_relative_to_src('../img/')
 
-list = []
-# Scan all file.jpg in img/
-for (root, dirs, file) in os.walk(path):
-	for f in file:
-		if '.jpg' in f:
-			list.append(f)
+def run(isLinux=False):
+	# Get the path of this file --> ~/fish-length-opencv/
+	path = get_path_relative_to_src('../img/')
 
-clear_json_file('result')
-for f in list:
-	# cmd /k : remain after run
-	# cmd /c : terminate after run
-	os.system('cmd /c "python main.py --image img/%s"' %f)
+	list = []
+	# Scan all file.jpg in img/
+	for (root, dirs, file) in os.walk(path):
+		for f in file:
+			if '.jpg' in f:
+				list.append(f)
 
-	# In this step, in result.json, "encoded": ""
-	# Add the encoded string to result.json for each image
-	add_encode_resultjson(key_name=f)
+	clear_json_file('result')
+	for f in list:
+		# cmd /k : remain after run
+		# cmd /c : terminate after run
+		if isLinux: 
+			os.system('python main.py --image img/%s' %f)
+		else: 	# For Windows
+			os.system('cmd /c "python main.py --image img/%s"' %f)
 
-# result.json will save all measurements result for each file in img/
-# Then validate it, only remain RESULT key
-validate_resultjson()
+		# In this step, in result.json, "encoded": ""
+		# Add the encoded string to result.json for each image
+		add_encode_resultjson(key_name=f)
+		print( str(' [OK] %s ' %f).center(42, '='), end='\n\n' )
 
-# From final result.json, I need to update final.bin & final.jpg
-update_files_from_resultjson()
+	# result.json will save all measurements result for each file in img/
+	# Then validate it, only remain RESULT key
+	validate_resultjson()
 
-# Print final result
-print_final_result()
+	# From final result.json, I need to update final.bin & final.jpg
+	update_files_from_resultjson()
+
+	# Print final result
+	print_final_result()
+
+if __name__ == '__main__':
+	run(isLinux=False)
